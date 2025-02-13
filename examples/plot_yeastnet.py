10f4d6afed14e8bc95cdd9e53136b5f0aa6471a7
Ross Barnowski <rossbar@caltech.edu>
2025-02-13 15:20:11 -0800

    WIP: Add basic examples, to be connected to sphinx-gallery.

---
+++ examples/plot_yeastnet.py
@@ -0,0 +1,22 @@
+"""
+Yeast Net
+"""
+import imageio.v3 as iio
+import napari
+
+from cellSAM.cellsam_pipeline import cellsam_pipeline
+img = iio.imread("../sample_imgs/YeastNet.png")
+
+mask = cellsam_pipeline(
+    img,
+    low_contrast_enhancement=False,
+    use_wsi=False,
+    visualize=False,
+    gauge_cell_size=False,
+)
+
+nim = napari.view_image(img, name="Yeast Net");
+nim.add_labels(mask, name="Cellsam segmentation");
+
+if __name__ == "__main__":
+    napari.run()
