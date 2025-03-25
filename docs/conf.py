47f37f3089cd7bcbdf17c579fff6c9d5f25ba56c
Ross Barnowski <rossbar@caltech.edu>
2025-03-24 18:15:46 -0700

    Add myst-nb to docs extensions.

--- docs/conf.py
+++ docs/conf.py
@@ -59,6 +59,7 @@ extensions = [
     "sphinx.ext.autodoc",  # Docstring summaries
     "sphinx_gallery.gen_gallery",  # Example gallery
     "sphinx.ext.intersphinx",  # Cross-project documentation linking
+    "myst_nb",  # Text-based jupyter notebooks
 ]
 
 sphinx_gallery_conf = {
