cfcfc1747c2a55305003fc1272f578cff6cd5dda
Ross Barnowski <rossbar@caltech.edu>
2025-08-04 05:22:02 -0700

    Add authentication layer for model weights (#12)
    * Add deepcell-users auth layer for weights.
    
    * Fixup model extraction code.
    
    * Update model to v1.1
    
    * DOC: Add blurb about model weights on users.deepcell.org

--- cellSAM/model.py
+++ cellSAM/model.py
@@ -1,12 +1,12 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from tqdm import tqdm
 
 from warnings import warn
 
 import requests
 import os
+from pathlib import Path
 import yaml
 import pkgutil
 from pkg_resources import resource_filename
@@ -29,23 +29,11 @@ from .utils import (
     fill_holes_and_remove_small_masks,
     subtract_boundaries,
 )
+from ._auth import fetch_data
 
 
-def download_file_with_progress(url, destination):
-    response = requests.get(url, stream=True)
-    total_size_in_bytes = int(response.headers.get("content-length", 0))
-    block_size = 1024  # 1 Kibibyte
+__all__ = ["segment_cellular_image"]
 
-    progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
-
-    with open(destination, "wb") as file:
-        for data in response.iter_content(block_size):
-            progress_bar.update(len(data))
-            file.write(data)
-    progress_bar.close()
-
-    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
-        print("ERROR: Something went wrong")
 
 def get_local_model(model_path: str) -> nn.Module:
     """
@@ -64,29 +52,18 @@ def get_model(model: nn.Module = None) -> nn.Module:
     """
     Returns a loaded CellSAM model. If model is None, downloads weights and loads the model with a progress bar.
     """
-    cellsam_assets_dir = os.path.join(os.path.expanduser("~"), ".cellsam_assets")
-    model_path = os.path.join(cellsam_assets_dir, "cellsam_base_v1.1.pt")
-
-    # path = pkg_resources.resource_filename('my.package', 'resource.dat')
-
-    # config_path = resource_filename(__name__, "modelconfig.yaml")
-
-    # with open(config_path, "r") as config_file:
-
-    config = yaml.safe_load(
-        pkgutil.get_data(__name__, "modelconfig.yaml")
-    )
-
+    cellsam_assets_dir = Path.home() / ".deepcell/models"
+    model_path = cellsam_assets_dir / "cellsam_base_v1.1.pt"
+    config_path = resource_filename(__name__, 'modelconfig.yaml')
+    with open(config_path, 'r') as config_file:
+        config = yaml.safe_load(config_file)
 
     if model is None:
-        if not os.path.exists(cellsam_assets_dir):
-            os.makedirs(cellsam_assets_dir)
-        if not os.path.isfile(model_path):
-            print("Downloading CellSAM model weights, please wait...")
-            download_file_with_progress(
-                "https://storage.googleapis.com/cellsam-data/cellsam_base.pt",
-                model_path,
-            )
+        if not cellsam_assets_dir.exists():
+            cellsam_assets_dir.mkdir(parents=True, exist_ok=True)
+        if not model_path.exists():
+            fetch_data("models/cellsam_base_v1.1.pt", cache_subdir="models")
+            assert model_path.exists()
         model = CellSAM(config)
         model.load_state_dict(torch.load(model_path, map_location="cpu"), strict=False)
     return model
